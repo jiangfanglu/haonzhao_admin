47c82cf5a79066dd3cf6505a46a626d8fad7def4
oldfritter <leon.zcf@gmail.com>
2014-03-16 12:01:21 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HaonzhaoAdmin::Application.config.secret_key_base = 'f60ca2c53c37914f3cb5424bcd9799c338a33f7f7424227737a9e90386d0b7a468f453ba1b29a32c72a90283ef14b951b0f3acc516fc41ccc1bc26dcd1d5d6f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HaonzhaoAdmin::Application.config.session_store :cookie_store, key: '_haonzhao_admin_session'
