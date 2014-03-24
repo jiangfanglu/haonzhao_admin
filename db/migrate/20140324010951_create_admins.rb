6c878fbadfe33bb7db9c4fc313cc7208858273ba
oldfritter <leon.zcf@gmail.com>
2014-03-24 19:20:24 +0800

    add salt

--- db/migrate/20140324010951_create_admins.rb
+++ db/migrate/20140324010951_create_admins.rb
@@ -2,7 +2,7 @@ class CreateAdmins < ActiveRecord::Migration
   def change
     create_table :admins do |t|
       
-      t.string :name, :username, :email, :password, :activation, :usertype, :token
+      t.string :name, :username, :email, :password, :activation, :usertype, :token, :salt
       t.integer :block, :sendEmail, :resetCount, :thumbnail
       t.datetime :registerDate, :lastvisitDate, :lastResetTime
       t.text :params
