05f01683d7d0b6dd443e245f915a73d0e8458200
oldfritter <leon.zcf@gmail.com>
2014-03-24 19:17:25 +0800

    up

--- db/migrate/20140324010951_create_admins.rb
+++ db/migrate/20140324010951_create_admins.rb
@@ -1,6 +1,11 @@
 class CreateAdmins < ActiveRecord::Migration
   def change
     create_table :admins do |t|
+      
+      t.string :name, :username, :email, :password, :activation, :usertype, :token
+      t.integer :block, :sendEmail, :resetCount, :thumbnail
+      t.datetime :registerDate, :lastvisitDate, :lastResetTime
+      t.text :params
 
       t.timestamps
     end
