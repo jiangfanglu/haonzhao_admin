7289e8613f833d751f3bfe23899d5fa8065297ef
oldfritter <leon.zcf@gmail.com>
2014-04-21 20:00:29 +0800

    fix bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 #encoding: utf-8
 class Order < ActiveRecord::Base
+  establish_connection :haonzhao
   attr_accessible :order_count, :total, :order_status_id, :shipping_option_id, :user_agent, :ip, :payment_sn,:order_sn, :shipping_code, :shipping_method
   self.table_name = 'oc_order'
+  self.primary_key = 'order_id'
+  belongs_to :shop, foreign_key: "store_id"
   
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,15 +2,16 @@ class Transaction < ActiveRecord::Base
   establish_connection :haonzhao
     
   belongs_to :settlement
-  belongs_to :sender, polymorphic: true 
-  belongs_to :receiver, polymorphic: true 
+  belongs_to :order, foreign_key: 'transaction_source_id', class_name: 'Order'
+  belongs_to :sender, polymorphic: true
+  belongs_to :receiver, polymorphic: true
     
   scope :paid_by_shop, ->(user_id) {where("(receiver_id = ? AND receiver_type = 'User' AND status = 'Paid') OR (sender_id = ? AND sender_type = 'Shop' AND status = 'Paid')", user_id, user_id)}
 
   scope :unpaid_by_shop, ->(user_id) {where("(receiver_id = ? AND receiver_type = 'User' AND status = 'Unpaid') OR (sender_id = ? AND sender_type = 'Shop' AND status = 'Unpaid')", user_id, user_id)}
   
   scope :all_by_shop, ->(store_id) {where("(receiver_id = ? AND receiver_type = 'User') OR (sender_id = ? AND sender_type = 'Shop')", store_id, store_id)}
-  
+    
   def pay
     update_attribute :status, 'Paid'
   end
