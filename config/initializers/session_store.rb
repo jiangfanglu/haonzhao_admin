8599acaa3a54931e068eec0c41e86bc977046b21
oldfritter <leon.zcf@gmail.com>
2014-05-07 15:43:50 +0800

    加入session过期时间

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # HaonzhaoAdmin::Application.config.session_store :cookie_store, key: '_haonzhao_admin_session'
-HaonzhaoAdmin::Application.config.session_store :active_record_store
+HaonzhaoAdmin::Application.config.session_store :active_record_store, {expire_after: 30.hours}
 ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
