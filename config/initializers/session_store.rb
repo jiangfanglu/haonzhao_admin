c4a773d4332bc5a7df4f8476c63674ab5922183c
oldfritter <leon.zcf@gmail.com>
2014-05-19 11:03:46 +0800

    time out

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # HaonzhaoAdmin::Application.config.session_store :cookie_store, key: '_haonzhao_admin_session'
-HaonzhaoAdmin::Application.config.session_store :active_record_store, {expire_after: 2.hours}
+HaonzhaoAdmin::Application.config.session_store :active_record_store, {:key => '_haonzhao_admin_session', :secret => '34292119910727272X', expire_after: 30.minutes}
 ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
