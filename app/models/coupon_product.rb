4a2e40de523ca25b4407ebbc78d1b385c91b2719
jiangfanglu <jiangfanglu@hotmail.com>
2014-05-19 10:39:43 +0800

    cp

--- app/models/coupon_product.rb
+++ app/models/coupon_product.rb
@@ -1,5 +1,6 @@
 class CouponProduct < ActiveRecord::Base
 	establish_connection :haonzhao
+	attr_accessible :coupon_id,:product_id
 	self.table_name = 'oc_coupon_product'
      self.primary_key = 'coupon_product_id'
 end
