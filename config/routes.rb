b223dc06a1de7f938e40b4540898c9954cc43fc2
root <root@AY1406251105298562efZ.(none)>
2014-07-02 17:56:10 +0800

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -88,7 +88,7 @@ HaonzhaoAdmin::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'shops#index'
+   root 'shops#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
