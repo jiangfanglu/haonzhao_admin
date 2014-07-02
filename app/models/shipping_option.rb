bcb3422fdb767b3a9baf109ce0ba593d92265542
oldfritter <leon.zcf@gmail.com>
2014-07-02 17:53:45 +0800

    fix bug

--- app/models/shipping_option.rb
+++ app/models/shipping_option.rb
@@ -1,5 +1,5 @@
 class ShippingOption < ActiveRecord::Base
 	  establish_connection :haonzhao
 	  attr_accessible :option_value, :shipping_id, :price
-	  self.table_name = 'oc_shopping_option'
+	  self.table_name = 'oc_shipping_option'
 end
