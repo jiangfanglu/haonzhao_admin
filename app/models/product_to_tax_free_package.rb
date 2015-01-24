34ea868268674a0e68d7bdafd4cbd7e50dc49b49
jiangfanglu <jiangfanglu@hotmail.com>
2015-01-24 23:36:32 +1100

    tax free package

--- app/models/product_to_tax_free_package.rb
+++ app/models/product_to_tax_free_package.rb
@@ -1,2 +1,4 @@
 class ProductToTaxFreePackage < ActiveRecord::Base
+	establish_connection :haonzhao
+	attr_accessible :package_id, :product_id,:quantity
 end
--- app/models/tax_free_package.rb
+++ app/models/tax_free_package.rb
@@ -1,2 +1,4 @@
 class TaxFreePackage < ActiveRecord::Base
+	establish_connection :haonzhao
+	attr_accessible :description, :name, :purchase_count, :viewed,:supplier_id,:meta_desctiption
 end
