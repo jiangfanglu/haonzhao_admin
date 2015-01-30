f32c2a4a9d21b3443b654b73ceec0cd202eb26ce
jiangfanglu <jiangfanglu@hotmail.com>
2015-01-30 12:20:39 +1100

    package_id
    package_id

--- app/models/content_product.rb
+++ app/models/content_product.rb
@@ -1,5 +1,5 @@
 class ContentProduct < ActiveRecord::Base
 	establish_connection :haonzhao
-	attr_accessible :product_id, :title, :meta_description, :country_code, :video, :tags, :alias, :max_price, :min_price
+	attr_accessible :product_id, :title, :meta_description, :country_code, :video, :tags, :alias, :max_price, :min_price,:package_id
 	has_many :content_product_images
 end
